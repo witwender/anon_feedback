14e53e7d82e2e9abec0d617602b748ea0559e95d
Wit Sumathavanit <wit.suma@gmail.com>
2023-04-10 06:27:19 +0000

    client & server validation and @-symbol sanitize

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,15 +1,19 @@
 class Feedback < ApplicationRecord
+
+  before_create :sanitize
+  validates_presence_of :recipient_handle, :text
+  validates_length_of :recipient_handle, :in => 1..15
+  validates_length_of :text, :in => 1..250
+
   after_create :send_tweet
+
+  def sanitize
+    # => insert logic regarding transformations
+    self.recipient_handle = self.recipient_handle.gsub('@', '')
+  end
 
   def send_tweet
     puts "Sending tweet"
     TwitterService.tweet!(self)
   end
-
-  private
-
-  #def feedback_params
-   # request.params
-    #params
-  #end
 end
